febb595a8f9a739b6491375db0d1bd5bc695d546
Mustafa Dag <mustafaa_dag@hotmail.com>
2020-10-08 12:54:16 -0400

    icons converted and service-worker.js file updated

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,7 +8,8 @@ var urlsToCache = [
   "/manifest.json",
   "/styles.css",
   "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png"
+  "/icons/icon-512x512.png",
+  "/index.html",
 ];
 
 self.addEventListener("install", function(event) {
